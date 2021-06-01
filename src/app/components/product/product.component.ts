80f634885460976fe5236b125a45a180bb69a967
chetansagta <tastyguava@gmail.com>
2021-06-01 23:12:15 +0530

    Added funcationality to adding products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,21 +2,27 @@ import { DataTransferService } from './../../service/dataTransferService';
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 
-@Component({
-  selector: 'product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
-})
+@Component({ selector: 'product', templateUrl: './product.component.html', styleUrls: ['./product.component.css'] })
 export class ProductComponent implements OnInit {
 
   @Input() url?: string;
 
-  currentProduct ?: Product;
+  currentProduct?: Product;
 
-  constructor(private dts : DataTransferService<Product>) { }
+  quantity = 1;
+
+  constructor(private dts: DataTransferService<Product>) { }
 
   ngOnInit(): void {
     console.log("Message from Home " + JSON.stringify(this.currentProduct = this.dts.getContent()));
-}
+  }
 
+  addProduct(count: number): void {
+    if (this.quantity > 0)
+      this.quantity += count;
+
+
+    if (this.quantity == 0)
+      this.quantity = 1;
+  }
 }
